80dd70f5f6dc0990f8f798c66982b3b341b7672d
serefbilge <serefbilge@gmail.com>
2014-05-31 22:44:07 +0300

    search box in detail page fix

--- H724/Controllers/DetailsController.cs
+++ H724/Controllers/DetailsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using H724.Models;
 
 namespace H724.Controllers
 {
@@ -13,16 +14,21 @@ namespace H724.Controllers
 
         public ActionResult Room()
         {
+            ViewBag.SearchType = SearchType.RoomSearch;
+
             return View();
         }
 
         public ActionResult Spa()
         {
+            ViewBag.SearchType = SearchType.SpaSearch;
+
             return View();
         }
 
         public ActionResult Restaurant(string id)
         {
+            ViewBag.SearchType = SearchType.RestaurantSearch;
             ViewBag.RestaurantId = id ?? "124"; 
 
             return View();
@@ -30,16 +36,22 @@ namespace H724.Controllers
 
         public ActionResult Pool()
         {
+            ViewBag.SearchType = SearchType.PoolSearch;
+
             return View();
         }
 
         public ActionResult Event()
         {
+            ViewBag.SearchType = SearchType.EventSearch;
+
             return View();
         }
 
-        public ActionResult ChangeSearch()
+        [ChildActionOnly]
+        public ActionResult ChangeSearch(SearchType searchType)
         {
+            ViewBag.SearchType = searchType;
             return PartialView("_ChangeSearch");
         }
     }
---
+++ H724/Models/SearchType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace H724.Models
+{
+    public enum SearchType
+    {
+        RoomSearch,
+        SpaSearch,
+        RestaurantSearch,
+        PoolSearch,
+        EventSearch
+    }
+}
