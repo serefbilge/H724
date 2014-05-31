741ebfe00a108af5b33d838c62c850fb2d8c01dd
serefbilge <serefbilge@gmail.com>
2014-05-31 22:03:52 +0300

    Search Boxes fix

---
+++ H724/Controllers/SearchController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace H724.Controllers
+{
+    public class SearchController : Controller
+    {
+        [ChildActionOnly]
+        public ActionResult RoomSearch()
+        {
+            return PartialView("_RoomSearch");
+        }
+        [ChildActionOnly]
+        public ActionResult SpaSearch()
+        {
+            return PartialView("_SpaSearch");
+        }
+        [ChildActionOnly]
+        public ActionResult RestaurantSearch()
+        {
+            return PartialView("_RestaurantSearch");
+        }
+        [ChildActionOnly]
+        public ActionResult PoolSearch()
+        {
+            return PartialView("_PoolSearch");
+        }
+        [ChildActionOnly]
+        public ActionResult EventSearch()
+        {
+            return PartialView("_EventSearch");
+        }
+
+    }
+}
